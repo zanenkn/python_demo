73d809bbb5be5db1e127a834f6933f88653d67cd
zanenkn <zane.neikena@gmail.com>
2019-10-18 13:40:09 +0200

    Installs flask_sqlalchemy, modifies app.py.

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Book
 
 @app.route("/")
 def hello():
     return "Hello World!"
-
-
-@app.route("/name/<name>")
-def get_book_name(name):
-    return "name : {}".format(name)
-
-
-@app.route("/details")
-def get_book_details():
-    author = request.args.get('author')
-    published = request.args.get('published')
-    return "Author : {}, Published: {}".format(author, published)
-
-
-if __name__ == '__main__':
-    app.run()
